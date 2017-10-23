3ff68e495de2e8b7f80da90caeb72acb877f02b3
Fernando Tessmann <nandotess85@gmail.com>
2017-10-23 14:23:18 -0500

    Coding the very first version

--- classes/class-lsx-videos-admin.php
+++ classes/class-lsx-videos-admin.php
@@ -67,6 +67,7 @@ class LSX_Videos_Admin {
 			'publicly_queryable' => true,
 			'show_ui'            => true,
 			'show_in_menu'       => true,
+			'show_in_nav_menus'  => true,
 			'menu_icon'          => 'dashicons-video-alt',
 			'query_var'          => true,
 			'rewrite'            => array(
@@ -111,6 +112,7 @@ class LSX_Videos_Admin {
 			'show_ui'           => true,
 			'show_admin_column' => true,
 			'query_var'         => true,
+			'show_in_nav_menus' => true,
 			'rewrite'           => array(
 				'slug' => 'videos-category',
 			),
