094e70d31f3c0d2095d71a2594400a77aec65a12
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2019-07-19 17:27:49 -0400

    removing gulp-minify-css and updating to gulp 4+

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const autoprefixer = require('gulp-autoprefixer');
 const concat       = require('gulp-concat');
 const gettext      = require('gulp-gettext');
 const jshint       = require('gulp-jshint');
-const minify       = require('gulp-minify-css');
 const plumber      = require('gulp-plumber');
 const rename       = require('gulp-rename');
 const rtlcss       = require('gulp-rtlcss');
@@ -25,7 +24,7 @@ gulp.task('default', function() {
 	console.log('gulp wordpress-lang            to compile the lsx-videos.pot, lsx-videos-en_EN.po and lsx-videos-en_EN.mo');
 });
 
-gulp.task('styles', function () {
+gulp.task('styles', function (done) {
 	return gulp.src('assets/css/scss/*.scss')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -43,10 +42,11 @@ gulp.task('styles', function () {
 			casacade: true
 		}))
 		.pipe(sourcemaps.write('maps'))
-		.pipe(gulp.dest('assets/css'))
+		.pipe(gulp.dest('assets/css')),
+		done();
 });
 
-gulp.task('styles-rtl', function () {
+gulp.task('styles-rtl', function (done) {
 	return gulp.src('assets/css/scss/*.scss')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -66,12 +66,15 @@ gulp.task('styles-rtl', function () {
 		.pipe(rename({
 			suffix: '-rtl'
 		}))
-		.pipe(gulp.dest('assets/css'))
+		.pipe(gulp.dest('assets/css')),
+		done();
 });
 
-gulp.task('compile-css', ['styles', 'styles-rtl']);
+gulp.task('compile-css',  gulp.series( ['styles', 'styles-rtl'] , function(done) {
+	done();
+}));
 
-gulp.task('js', function() {
+gulp.task('js', function(done) {
 	return gulp.src('assets/js/src/lsx-videos.js')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -83,10 +86,11 @@ gulp.task('js', function() {
 		//.pipe(errorreporter)
 		.pipe(concat('lsx-videos.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js'))
+		.pipe(gulp.dest('assets/js')),
+		done();
 });
 
-gulp.task('admin-js', function() {
+gulp.task('admin-js', function(done) {
 	return gulp.src('assets/js/src/lsx-videos-admin.js')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -98,10 +102,11 @@ gulp.task('admin-js', function() {
 		//.pipe(errorreporter)
 		.pipe(concat('lsx-videos-admin.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js'))
+		.pipe(gulp.dest('assets/js')),
+		done();
 });
 
-gulp.task('scporder-js', function() {
+gulp.task('scporder-js', function(done) {
 	return gulp.src('assets/js/src/scporder.js')
 		.pipe(plumber({
 			errorHandler: function(err) {
@@ -113,22 +118,29 @@ gulp.task('scporder-js', function() {
 		//.pipe(errorreporter)
 		.pipe(concat('scporder.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js'))
+		.pipe(gulp.dest('assets/js')),
+		done();
 });
 
-gulp.task('compile-js', ['js', 'admin-js', 'scporder-js']);
+gulp.task('compile-js', gulp.series( ['js', 'admin-js', 'scporder-js'] , function(done) {
+	done();
+}));
 
-gulp.task('watch-css', function () {
-	return gulp.watch('assets/css/**/*.scss', ['compile-css']);
+gulp.task('watch-css', function (done) {
+	done();
+	return gulp.watch('assets/css/**/*.scss', gulp.series('compile-css'));
 });
 
-gulp.task('watch-js', function () {
-	return gulp.watch('assets/js/src/**/*.js', ['compile-js']);
+gulp.task('watch-js', function (done) {
+	done();
+	return gulp.watch('assets/js/src/**/*.js', gulp.series('compile-js'));
 });
 
-gulp.task('watch', ['watch-css', 'watch-js']);
+gulp.task('watch', gulp.series( ['watch-css', 'watch-js'], function(done) {
+	done();
+}));
 
-gulp.task('wordpress-pot', function() {
+gulp.task('wordpress-pot', function(done) {
 	return gulp.src('**/*.php')
 		.pipe(sort())
 		.pipe(wppot({
@@ -137,10 +149,11 @@ gulp.task('wordpress-pot', function() {
 			bugReport: 'https://github.com/lightspeeddevelopment/lsx-videos/issues',
 			team: 'LightSpeed <webmaster@lsdev.biz>'
 		}))
-		.pipe(gulp.dest('languages/lsx-videos.pot'))
+		.pipe(gulp.dest('languages/lsx-videos.pot')),
+		done();
 });
 
-gulp.task('wordpress-po', function() {
+gulp.task('wordpress-po', function(done) {
 	return gulp.src('**/*.php')
 		.pipe(sort())
 		.pipe(wppot({
@@ -152,10 +165,13 @@ gulp.task('wordpress-po', function() {
 		.pipe(gulp.dest('languages/lsx-videos-en_EN.po'))
 });
 
-gulp.task('wordpress-po-mo', ['wordpress-po'], function() {
+gulp.task('wordpress-po-mo', gulp.series( ['wordpress-po'], function(done) {
 	return gulp.src('languages/lsx-videos-en_EN.po')
 		.pipe(gettext())
-		.pipe(gulp.dest('languages'))
-});
+		.pipe(gulp.dest('languages')),
+		done();
+}));
 
-gulp.task('wordpress-lang', (['wordpress-pot', 'wordpress-po-mo']));
+gulp.task('wordpress-lang', gulp.series( ['wordpress-pot', 'wordpress-po-mo'] , function(done) {
+	done();
+}));
